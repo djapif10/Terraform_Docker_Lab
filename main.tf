4cfd87ceec81daed9e1bd2a3cc602a6bbe965dce
cloudtech-services <djapif@gmail.com>
2022-11-14 12:15:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "techmax" {
-  name         = "fdjapi10/techmax:v1.0.0"
+  name         = "fdjapi10/techmax"
   keep_locally = false
 }
 
 resource "docker_container" "TechmaxPro" {
-  image = docker_image.techmax.v1.0.0
+  image = docker_image.techmax
   name  = "E-Commerce"
   ports {
     internal = 80
